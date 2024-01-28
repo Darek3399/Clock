b2b1ab677163f6d19112f6024aa3ff1852e4059f
Darek3399 <darek3399@mail.ru>
2024-01-28 15:46:53 +0300

    +

--- script.js
+++ script.js
@@ -10,18 +10,17 @@ const minNumCont = document.querySelector('.minutes').querySelectorAll('.num')
 const hoursNumCont = document.querySelector('.hours').querySelectorAll('.num')
 
 const deg = 360 / 60
-const degHours = 360 / 60
+const degHours = 360 / 12
 
 setInterval(() => {
     let seconds = new Date().getSeconds(),
     minutes = new Date().getMinutes(),
-    hours = new Date().getHours()
-
+    hours = new Date().getHours() + 3
 
 
     secCont.setAttribute('style', `transform: rotateZ(${seconds * deg}deg);`)
     minCont.setAttribute('style', `transform: rotateZ(${minutes * deg}deg);`)
-    hoursCont.setAttribute('style', `transform: rotateZ(${hours * degHours}deg);`)
+    hoursCont.setAttribute('style', `transform: rotateZ(${(hours - 12) * degHours}deg);`)
 
     for (let i = 0; i < secNumCont.length; i++) {
         secNumCont[i].innerHTML = `${seconds}`
@@ -35,11 +34,7 @@ setInterval(() => {
 
     for (let i = 0; i < hoursNumCont.length; i++) {
         hoursNumCont[i].innerHTML = `${hours}`
-        hoursNumCont[i].setAttribute('style', `transform: rotateZ(${ - hours * deg}deg);`)
+        hoursNumCont[i].setAttribute('style', `transform: rotateZ(${ - hours * degHours}deg);`)
     }
-
-
-
-
-
+    
 }, 1000)
